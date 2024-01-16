036e7cb93c5144ac2bae22a800f456a9efcdf528
OMCbusiness <151764545+OMCbusiness@users.noreply.github.com>
2024-01-16 16:59:13 +0100

    Mensaje descriptivo del commit

---
+++ medical_jlm/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Medical application',
+    'version': '15.0.1.0.0',
+    'summary': 'Management of patients and therapists.',
+    'category': 'Contacts',
+    'author': 'Javier L. de los Mozos',
+    'maintainer': 'Javier L. de los Mozos',
+    'depends': ['contacts', 'resource', 'survey_crm_generation', 'sales_team'],
+    'data': [
+        'views/medical_views.xml',
+        'views/partner_views.xml',
+        'views/therapist_calendar_views.xml',
+        'views/survey_survey_patient_views.xml',
+        'views/survey_survey_partner_views.xml',
+        'views/crm_menu_views.xml',
+        'security/ir.model.access.csv',
+        'security/security.xml',
+    ],
+    'images': [],
+    'license': 'Other proprietary',
+    'installable': True,
+    'application': True
+}
