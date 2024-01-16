9bf1b01a950a65e18ce8564f2b72fe7707d8ae21
OMCbusiness <151764545+OMCbusiness@users.noreply.github.com>
2024-01-16 18:56:40 +0100

    Mensaje descriptivo del commit

---
+++ cheats/__manifest__ (2).py
@@ -0,0 +1,18 @@
+{
+    'name': 'Cheats',
+    'version': '1.0',
+    'summary': 'Module summary',
+    'description': 'Module description',
+    'category': 'Uncategorized',
+    'author': 'Your Name',
+    'website': 'https://www.example.com',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/fake_user_registry_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
