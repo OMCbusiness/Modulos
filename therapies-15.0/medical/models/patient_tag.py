3acce7641f925f74c28887d21476c611e27e0c0d
OMCbusiness <151764545+OMCbusiness@users.noreply.github.com>
2024-01-24 19:26:10 +0100

    Mensaje descriptivo del commit

--- therapies-15.0/medical/models/patient_tag.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class PatientTag(models.Model):
-    _name = "medical.patient.tag"
-    _description = "Tags to assign to patients."
-    
-    name = fields.Char()
-    color = fields.Integer()
-    active = fields.Boolean(default=True)
